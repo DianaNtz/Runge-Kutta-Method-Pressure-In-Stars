4b04aa1cf6c1c2e741a081aea7832d0005b4cce3
Diana Nitzschke <diana.nitzschke@stud.uni-hannover.de>
2022-08-14 19:58:49 +0200

    set up initial values

--- Newton.py
+++ Newton.py
@@ -3,3 +3,11 @@
 """
 import numpy as np
 import matplotlib.pyplot as plt
+dr=100 #in m
+R=30000 #stars radius in m
+Rfinal=R
+r0=dr
+steps=int(-(r0-Rfinal)/dr)
+rho0=6*10**16 #central density of the star in kg/m^3
+G=6.67259*10**-11 #gravitational constant in m^3/(kg s^2)
+P0=4*np.pi/3*rho0*R**3*G/(2*R)*rho0 #central pressure (Newton)
