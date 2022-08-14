4d6d555bfa1af26cb8b82ab71a3490dd6bdb20b2
Diana Nitzschke <diana.nitzschke@stud.uni-hannover.de>
2022-08-14 20:41:43 +0200

    started loop for iteration and plotted numerical vs analytical results

--- Lane.py
+++ Lane.py
@@ -25,3 +25,35 @@ def Mass(r,rho0,Parr):
 def f(r,P,Parr):   
     Lane=-G/r**2*Mass(r,rho0,Parr)*rho(r, rho0, P)
     return Lane
+P=np.empty(steps+1, dtype='double')
+r=np.empty(steps+1, dtype='double')
+Pn=P0
+rn=r0
+#Runge Kutta fourth
+for i in range(0,steps+1):
+    r[i]=rn
+    P[i]=Pn
+    k1=dr*f(rn,Pn,P)
+    k2=dr*f(rn+0.5*dr,Pn+0.5*k1,P)
+    k3=dr*f(rn+0.5*dr,Pn+0.5*k2,P)
+    k4=dr*f(rn+dr,Pn+k3,P)
+    rn=rn+dr
+    Pn=Pn+k2*(2/6)+k1*(1/6)+k3*(2/6)+k4*(1/6)
+#analytical solution of the Lane Emden equation for n=1  
+gamma=1/n+1
+zeta=np.sqrt(4*np.pi*G*(gamma-1)/(k*gamma))*r*rho0**((2-gamma)/2)
+theta=np.sin(zeta)/zeta
+rhoa=theta**n*rho0
+Pa=k*rhoa**gamma
+ax1 = plt.subplots(1, sharex=True, figsize=(10,5))
+plt.plot(r/1000,Pa,color='black',linestyle='-',linewidth=2.5,label = "$P_a(r)$ ")          
+plt.plot(r/1000,P,color='deepskyblue',linestyle='-.',linewidth=2.5,label="$P(r)$ ")
+plt.xlabel("distance $r$ in [km]",fontsize=19) 
+plt.ylabel(r'pressure $P$ in [N/$m^2$]',fontsize=19,labelpad=10)
+plt.xlim([r0/1000,Rfinal/1000])
+plt.ylim([0,P0+P0*0.13])  
+plt.xticks(fontsize= 17)
+plt.yticks(fontsize= 17)
+plt.legend(loc=1,fontsize=19,handlelength=3) 
+plt.savefig("Lane.png",dpi=250)
+plt.show()
