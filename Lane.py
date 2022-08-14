e1f19d913a04807e5c16715121036a23a60713ad
Diana Nitzschke <diana.nitzschke@stud.uni-hannover.de>
2022-08-14 20:38:35 +0200

    set up functions

--- Lane.py
+++ Lane.py
@@ -13,3 +13,15 @@ G=6.67259*10**-11 #gravitational constant in m^3/(kg s^2)
 n=1 #dimensionless
 k=2 #m^5/kg/s^2 for n=1
 P0=k*rho0**(1+1/n) #central pressure 
+def rho(r,rho0,P):
+    return ((1/k)*P)**(n/(n+1))   
+def Mass(r,rho0,Parr):
+    n=int((r-r0)/dr)
+    rint=np.linspace(r0,r,n) 
+    M=0
+    for i in range(0,n):
+        M=4*np.pi*rint[i]**2*rho(rint[i],rho0,Parr[i])*dr+M 
+    return M
+def f(r,P,Parr):   
+    Lane=-G/r**2*Mass(r,rho0,Parr)*rho(r, rho0, P)
+    return Lane
